ffaab9c6e709c3cd34014e700aa02288047e5e91
Cecilia Marcellesi <ceciliamarcellesi@gmail.com>
2019-11-20 10:59:45 +0000

    Added latitude and longitude columns to flats

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_135948) do
+ActiveRecord::Schema.define(version: 2019_11_20_104733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,8 @@ ActiveRecord::Schema.define(version: 2019_11_19_135948) do
     t.string "location"
     t.integer "price"
     t.bigint "user_id"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
