45a7b7bfcdaab57ba429529006dc8d30b679f34a
Cecilia Marcellesi <ceciliamarcellesi@gmail.com>
2019-11-21 12:29:15 +0000

    Fixed dynamic text

--- app/javascript/packs/components/banner.js
+++ app/javascript/packs/components/banner.js
@@ -3,7 +3,8 @@ import Typed from 'typed.js';
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
     strings: ["Book unique cribs", "Live unique experiences"],
-    typeSpeed: 50,
+    contentType: 'text',
+    typeSpeed: 70,
     loop: true
   });
 }
