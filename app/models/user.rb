0ee50b7dcf3a0319fa009135f589a7e6b07ab570
Molly Daguilar <mdaguilar66@gmail.com>
2019-11-21 15:18:20 +0000

    added profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :bookings
+  has_one_attached :avatar
+
   # has_many :flats, through: :bookings
   has_many :flats
   # Include default devise modules. Others available are:
